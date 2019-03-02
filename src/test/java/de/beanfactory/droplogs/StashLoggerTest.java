7cc32c86e0e03f4d46cfd6adaa7c8a5e511eff4d
Thomas Strauß <thomas.strauss@pentasys.de>
2019-03-02 16:00:33 +0100

    fix compile error

--- src/test/java/de/beanfactory/droplogs/StashLoggerTest.java
+++ src/test/java/de/beanfactory/droplogs/StashLoggerTest.java
@@ -1,17 +1,12 @@
 package de.beanfactory.droplogs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyString;
 
 public class StashLoggerTest {
 
     @Test
-    public void dropLog() {
+    public void dropLog() throws JsonProcessingException {
         StashLogger logger = new StashLogger();
 
         logger.dropLog();
